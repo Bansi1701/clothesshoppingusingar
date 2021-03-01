990fa928ac92380c8b7b8bae3278c0f6849f1e27
My name <me@example.com>
2021-03-01 21:29:07 +0530

    login is added and security also.

--- base/com/controller/__init__.py
+++ base/com/controller/__init__.py
@@ -1,6 +1,6 @@
 from base.com.controller import area_controller
 from base.com.controller import category_controller
 from base.com.controller import city_controller
-from base.com.controller import home_controller
+from base.com.controller import login_controller
 from base.com.controller import state_controller
 from base.com.controller import subcategory_controller
