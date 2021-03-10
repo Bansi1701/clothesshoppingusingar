c65a9ee6758cde4582cbbd71874bd9fa987741df
bansi <bmpatel1701@gmail.com>
2021-03-10 23:22:29 +0530

    bansi

--- base/com/controller/__init__.py
+++ base/com/controller/__init__.py
@@ -6,3 +6,4 @@ from base.com.controller import state_controller
 from base.com.controller import subcategory_controller
 from base.com.controller import register_controller
 from base.com.controller import complaint_feedback_controller
+from base.com.controller import profile_controller
