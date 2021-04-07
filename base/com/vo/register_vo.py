a4ff6bd95f18fc5f0305e2521742445a20400745
mauli patel <mpatel91@horizon.csueastbay.edu>
2021-04-07 11:55:37 +0530

    register vo

---
+++ base/com/vo/register_vo.py
@@ -0,0 +1,34 @@
+from base import database
+from base.com.vo.city_vo import CityVO
+from base.com.vo.state_vo import StateVO
+from base.com.vo.login_vo import LoginVO
+
+
+class RegisterVO(database.Model):
+    __tablename__ = 'user_table'
+    user_id = database.Column('user_id', database.Integer, primary_key=True, autoincrement=True)
+    user_firstname = database.Column('user_firstname', database.String(100))
+    user_lastname = database.Column('user_lastname', database.String(100))
+    user_gender = database.Column('user_gender', database.String(100))
+    user_address= database.column('user_address',database.String(255))
+    user_pincode = database.Column('user_pincode', database.Integer)
+    user_state_id = database.Column('user_state_id', database.Integer, database.ForeignKey(StateVO.state_id))
+    user_city_id = database.Column('user_city_id', database.Integer, database.ForeignKey(CityVO.city_id))
+    user_login_id = database.Column('user_login_id', database.Integer, database.ForeignKey(LoginVO.login_id))
+
+
+    def as_dict(self):
+        return {
+            'user_id': self.user_id,
+            'user_firstname': self.user_firstname,
+            'user_lastname': self.user_lastname,
+            'user_gender': self.user_gender,
+            'user_address': self.user_address,
+            'user_pincode': self.user_pincode,
+            'user_state_id': self.user_state_id,
+            'user_city_id': self.user_city_id,
+            'user_login_id': self.user_login_id
+        }
+
+
+database.create_all()
